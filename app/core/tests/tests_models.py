59c08715ba37bc5b2a92b6d810868bd2fe76e3f1
Alireza <shahali.alireza.92@gmail.com>
2020-04-06 17:59:43 +0430

    adding superuser creation

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -13,3 +13,22 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_create_email_with_lowercase_URL(self):
+        email = "moreass@BOOBS.com"
+        password = "test12345"
+
+        user = get_user_model().objects.create_user(email = email, password= password)
+
+        self.assertEqual(user.email, email.lower())
+
+
+    def test_email_provided(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email = None, password = '123456')
+
+    
+    def test_creating_super_user(self):
+        user = get_user_model().objects.create_superuser(email = 'bitches@gmail.com', password= "123456")
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
